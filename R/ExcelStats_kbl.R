bdb3dea862ec0d6b42abffd70266506c3569e1e4
Karl-Gunnar Severinsen <kgse@hvl.no>
2025-01-29 17:03:04 +0100

    Added dependencies and cleaned code

--- R/ExcelStats_kbl.R
+++ R/ExcelStats_kbl.R
@@ -1,27 +1,4 @@
-library(dplyr)
-library(tibble)
-library(tidyr)
-library(e1071)  # For skewness and kurtosis
-library(modeest)  # For mode
-library(knitr)
-library(kableExtra)
-
-# Custom function to format numbers
-format_number <- function(x) {
-  if (is.numeric(x)) {
-    return(formatC(x, format = "f", digits = 6, drop0trailing = TRUE))
-  }
-  return(x)
-}
-
-# Custom function to handle mode
-get_mode <- function(data) {
-  mode_value <- suppressWarnings(mfv(data))
-  if (length(mode_value) > 1) {
-    return(NA)  # Return NA if there are multiple modes
-  }
-  return(mode_value)
-}
+# source("R/helpers.R")
 
 #' Calculate Descriptive Statistics as a Kable
 #'
---
+++ R/helpers.R
@@ -0,0 +1,24 @@
+#' @import dplyr
+#' @import tibble
+#' @import tidyr
+#' @import e1071
+#' @import modeest
+#' @import knitr
+#' @import kableExtra
+
+# Custom function to format numbers
+format_number <- function(x) {
+  if (is.numeric(x)) {
+    return(formatC(x, format = "f", digits = 6, drop0trailing = TRUE))
+  }
+  return(x)
+}
+
+# Custom function to handle mode
+get_mode <- function(data) {
+  mode_value <- suppressWarnings(mfv(data))
+  if (length(mode_value) > 1) {
+    return(NA)  # Return NA if there are multiple modes
+  }
+  return(mode_value)
+}
